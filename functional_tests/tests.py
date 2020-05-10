1ef8b23845bab11d2e05ff7b58f5b45d3ecc1e63
Makarov Andrey <winandfx@gmail.com>
2020-05-10 13:40:34 +0300

    User story typo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -92,7 +92,7 @@ class NewVisitorTest(LiveServerTestCase):
             self.browser.quit()
         self.browser = webdriver.Chrome()
 
-        # Francis visits the home page. There is no sing of Edith's
+        # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
