399b86c7094f51d4b41b725bd494b4e61debec74
Andrey Makarov <winandfx@gmail.com>
2020-10-14 19:59:41 +0000

    Fix: cannot put text into field in Chrome immediately after page load (when CSS is broken)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,6 +33,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def set_text(self, el, text):
+        """
+        Clear field and set text until success.
+        `send_keys` fails in Chrome if called too early.
+        """
+        while True:
+            el.clear()
+            el.send_keys(text)
+            if el.get_attribute("value") == text:
+                break
+            time.sleep(0.5)
+
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -63,7 +75,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make fly')
+        self.set_text(inputbox, 'Use peacock feathers to make fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
