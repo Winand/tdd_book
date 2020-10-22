11d19640680e9be76c658819cfe82516119f8891
Makarov Andrey <winandfx@gmail.com>
2020-10-22 22:50:54 +0300

    fix strange error in the end of line
    unsatisfied dependency "ev"

--- .vscode/env_alpine.sh
+++ .vscode/env_alpine.sh
@@ -7,7 +7,7 @@ ln -s /usr/bin/python3 /usr/bin/python
 pip3 install --upgrade pip
 pip install -r ../requirements.txt  # pip install django gunicorn
 pip install selenium
-apk add gcc python3-dev musl-dev libffi-dev make openssl-dev
+apk add gcc python3-dev musl-dev libffi-dev make openssl-dev #
 pip install fabric3
 pip install pylint
 apk add git
