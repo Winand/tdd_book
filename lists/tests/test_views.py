72b5674cdbe7a59cf60e0dfc4308755ca3486eb4
Makarov Andrey <winandfx@gmail.com>
2020-11-05 18:35:33 +0000

    fix typo in test_views

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -56,7 +56,7 @@ class NewListTest(TestCase):
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
-        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
