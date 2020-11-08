697dd32dfc18d6e0d2f1e8a46a0ccd2eeab47882
Makarov Andrey <winandfx@gmail.com>
2020-11-08 21:10:53 +0000

    Fix test name (ch.4)

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,7 @@ from django.utils.html import escape
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
