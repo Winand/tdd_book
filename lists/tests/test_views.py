8bcb3968b98821cd18bbd75a867b4649ea84840f
Makarov Andrey <winandfx@gmail.com>
2021-03-18 18:27:57 +0300

    Attempt to test owner field with mocks

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,4 @@
-from unittest import skip
+from unittest.mock import patch
 from django.test import TestCase, client
 from django.utils.html import escape
 from lists.forms import (
@@ -145,12 +145,25 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_list_owner_is_saved_if_user_is_authenticated(self):
+    @patch('lists.views.List')
+    @patch('lists.views.ItemForm')
+    def test_list_owner_is_saved_if_user_is_authenticated(
+        self, mockItemFormClass, mockListClass
+    ):
         user = User.objects.create(email='a@b.com')
         self.client.force_login(user)
+        mock_list = mockListClass.return_value
+        # Django 2+ http://disq.us/p/1q1k27x
+        mock_list.get_absolute_url.return_value = 'fakeurl'
+
+        def check_owner_assigned():
+            self.assertEqual(mock_list.owner, user)
+        mock_list.save.side_effect = check_owner_assigned
+
         self.client.post('/lists/new', data={'text': 'new item'})
-        list_ = List.objects.first()
-        self.assertEqual(list_.owner, user)
+
+        mock_list.save.assert_called_once_with()
+
 
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
