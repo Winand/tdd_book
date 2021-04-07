96d84ab01148343064e5337275993b107c892c0b
Andrey Makarov <winandfx@gmail.com>
2021-04-07 09:05:56 +0300

    pip explicit install

--- .vscode/env_alpine.sh
+++ .vscode/env_alpine.sh
@@ -2,12 +2,13 @@
 # pip packages are also installed with root permissions
 # otherwise they will be installed in ~/.local which is not in PATH
 apk add libstdc++  # required by VS Code
-apk add python3  # pip3 is also installed
+apk add python3
 ln -s /usr/bin/python3 /usr/bin/python
+apk add py3-pip
 pip3 install --upgrade pip
 pip install -r ../requirements.txt  # pip install django gunicorn
 pip install selenium
-apk add gcc python3-dev musl-dev libffi-dev make openssl-dev #
+apk add gcc python3-dev musl-dev libffi-dev make openssl-dev
 pip install fabric3
 pip install pylint
-apk add git #
+apk add git
