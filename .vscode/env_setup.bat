8533630e29fe7d963c9b5ca12672a50b4fb855fb
Andrey Makarov <winandfx@gmail.com>
2021-04-16 10:56:12 +0300

    Use .gitattributes instead of autocrlf setting
    https://www.edwardthomson.com/blog/advent_day_21_renormalizing_line_endings.html

--- .vscode/env_setup.bat
+++ .vscode/env_setup.bat
@@ -1,3 +1,3 @@
 wsl -d Alpine su -c "sh env_alpine.sh"
 REM Settings for non-root user:
-wsl -d Alpine git config --global core.autocrlf true
+REM wsl -d Alpine git config --global core.autocrlf true
